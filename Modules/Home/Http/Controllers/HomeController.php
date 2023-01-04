ed63431fbb996ca5da7b276b489506964411c926
Mohamadreza Rezaei <clash.trial@gmail.com>
2023-01-05 00:00:24 +0330

    add controllers and models for all modules

--- Modules/Home/Http/Controllers/HomeController.php
+++ Modules/Home/Http/Controllers/HomeController.php
@@ -1,14 +1,34 @@
 <?php
 
-namespace Modules\Home\Http\Controllers;
+namespace App\Http\Controllers\Customer;
 
-use Modules\Share\Http\Controllers\Controller;
+use App\Models\Market\Brand;
+use Illuminate\Http\Request;
+use App\Models\Content\Banner;
+use App\Models\Market\Product;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    public function index()
+
+
+    public function home()
     {
-        dd(1);
-        return view('Home::index');
+        Auth::loginUsingId(15);
+
+        $slideShowImages = Banner::where('position', 0)->where('status', 1)->get();
+        $topBanners = Banner::where('position', 1)->where('status', 1)->take(2)->get();
+        $middleBanners = Banner::where('position', 2)->where('status', 1)->take(2)->get();
+        $bottomBanner = Banner::where('position', 3)->where('status', 1)->first();
+
+        $brands = Brand::all();
+
+        $mostVisitedProducts = Product::latest()->take(10)->get();
+        $offerProducts = Product::latest()->take(10)->get();
+        return view('customer.home', compact('slideShowImages', 'topBanners', 'middleBanners', 'bottomBanner', 'brands', 'mostVisitedProducts', 'offerProducts'));
+
     }
+
+
 }
--- Modules/Payment/Http/Controllers/PaymentController.php
+++ Modules/Payment/Http/Controllers/PaymentController.php
@@ -1,14 +1,49 @@
 <?php
 
-namespace Modules\Payment\Http\Controllers;
+namespace App\Http\Controllers\Admin\Market;
 
-use Modules\Share\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Market\Payment;
+use App\Http\Controllers\Controller;
 
 class PaymentController extends Controller
 {
     public function index()
     {
-        dd(1);
-        return view('Payment::index');
+        $payments = Payment::all();
+        return view('admin.market.payment.index', compact('payments'));
+    }
+    public function offline()
+    {
+        $payments = Payment::where('paymentable_type', 'App\Models\Market\OfflinePayment')->get();
+        return view('admin.market.payment.index', compact('payments'));
+    }
+    public function online()
+    {
+        $payments = Payment::where('paymentable_type', 'App\Models\Market\OnlinePayment')->get();
+        return view('admin.market.payment.index', compact('payments'));
+    }
+    public function cash()
+    {
+        $payments = Payment::where('paymentable_type', 'App\Models\Market\CashPayment')->get();
+        return view('admin.market.payment.index', compact('payments'));
+    }
+    public function canceled(Payment $payment)
+    {
+        $payment->status = 2;
+        $payment->save();
+        return redirect()->route('admin.market.payment.index')->with('swal-success', 'تغییر شما با موفقیت انجام شد');
+    }
+
+    public function returned(Payment $payment)
+    {
+        $payment->status = 3;
+        $payment->save();
+        return redirect()->route('admin.market.payment.index')->with('swal-success', 'تغییر شما با موفقیت انجام شد');
+    }
+
+    public function show(Payment $payment)
+    {
+        return view('admin.market.payment.show', compact('payment'));
     }
 }
