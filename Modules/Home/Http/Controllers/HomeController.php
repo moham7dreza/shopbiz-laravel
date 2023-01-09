96335dac7afc5f645708aeaf326aac0009e310a2
Mohamadreza Rezaei <clash.trial@gmail.com>
2023-01-09 21:35:37 +0330

    load customer home page queries from repo

--- Modules/Home/Http/Controllers/HomeController.php
+++ Modules/Home/Http/Controllers/HomeController.php
@@ -5,12 +5,7 @@ namespace Modules\Home\Http\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Auth;
-use Modules\Banner\Entities\Banner;
-use Modules\Brand\Entities\Brand;
 use Modules\Home\Repositories\HomeRepoEloquent;
-use Modules\Home\Repositories\HomeRepoEloquentInterface;
-use Modules\Product\Entities\Product;
 use Modules\Share\Http\Controllers\Controller;
 
 class HomeController extends Controller
@@ -27,20 +22,7 @@ class HomeController extends Controller
      */
     public function home()
     {
-        Auth::loginUsingId(1);
-
-        $slideShowImages = Banner::query()->where('position', 0)->where('status', 1)->get();
-        $topBanners = Banner::query()->where('position', 1)->where('status', 1)->take(2)->get();
-        $middleBanners = Banner::query()->where('position', 2)->where('status', 1)->take(2)->get();
-        $bottomBanner = Banner::query()->where('position', 3)->where('status', 1)->first();
-
-        $brands = Brand::all();
-
-        $mostVisitedProducts = Product::query()->latest()->take(10)->get();
-        $offerProducts = Product::query()->latest()->take(10)->get();
-        return view('Home::home', compact('slideShowImages', 'topBanners', 'middleBanners', 'bottomBanner', 'brands', 'mostVisitedProducts', 'offerProducts'));
-
+        $repo = $this->homeRepo;
+        return view('Home::home', compact(['repo']));
     }
-
-
 }
--- Modules/Home/Repositories/HomeRepoEloquent.php
+++ Modules/Home/Repositories/HomeRepoEloquent.php
@@ -5,9 +5,64 @@ namespace Modules\Home\Repositories;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Banner\Entities\Banner;
+use Modules\Brand\Entities\Brand;
+use Modules\Product\Entities\Product;
 
 class HomeRepoEloquent implements HomeRepoEloquentInterface
 {
+    /**
+     * @return Builder[]|Collection
+     */
+    public function slideShowImages()
+    {
+        return Banner::query()->where('position', 0)->where('status', 1)->get();
+    }
+
+    /**
+     * @return Builder[]|Collection
+     */
+    public function topBanners()
+    {
+        return Banner::query()->where('position', 1)->where('status', 1)->take(2)->get();
+    }
+
+    /**
+     * @return Builder[]|Collection
+     */
+    public function middleBanners()
+    {
+        return Banner::query()->where('position', 2)->where('status', 1)->take(2)->get();
+    }
+
+    /**
+     * @return Builder|Model|object|null
+     */
+    public function bottomBanner()
+    {
+        return Banner::query()->where('position', 3)->where('status', 1)->first();
+    }
+
+    /**
+     * @return Builder[]|Collection
+     */
+    public function brands()
+    {
+        return Brand::query()->latest()->get();
+    }
+
+    /**
+     * @return Builder[]|Collection
+     */
+    public function mostVisitedProducts()
+    {
+        return Product::query()->latest()->take(10)->get();
+    }
+
+    public function offerProducts()
+    {
+        return Product::query()->latest()->take(10)->get();
+    }
     /**
      * Get the latest roles with permissions.
      *
